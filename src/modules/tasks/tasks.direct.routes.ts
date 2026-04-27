e5507d548a806c0f17712a6c6639a1f7d51fd812
Andres M <andresmaldonado200338@gmail.com>
2026-04-26 21:19:15 -0500

    fix: apply rate limiter before authenticate in tasks.direct.routes

--- src/modules/tasks/tasks.direct.routes.ts
+++ src/modules/tasks/tasks.direct.routes.ts
@@ -5,8 +5,9 @@ import * as ctrl from './tasks.controller'
 
 const router = Router()
 
-router.use(authenticate)
 router.use(generalRateLimiter)
+router.use(authenticate)
+
 // Montado en /api/tasks
 router.patch('/:id', ctrl.updateTask)
 router.delete('/:id', ctrl.deleteTask)
